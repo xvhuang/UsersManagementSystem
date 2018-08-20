f74769c02437e34d08c515b295b91e79ba72dc70
xvhuang <41632372+xvhuang@users.noreply.github.com>
2018-08-20 10:45:04 +0800

    Update About.vue

--- vcustomers/src/components/About.vue
+++ vcustomers/src/components/About.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about container">
-    <h1 class="page-header">希望能够提供给大家更优,更有用的课程!</h1>
-    <p>如果大家需要本课程的代码,可以加我们的QQ群,向我们索取!</p>
+    <h1 class="page-header">这是我的第一个学习项目，请多关照</h1>
+    <p>风正潮平，自当扬帆破浪；任重道远，更须快马加鞭！</p>
   </div>
 </template>
 
